0396f5cc2f75a7c55c6e4b19b7b116d40662320e
NPVinhLoc <vinhloc4927@gmail.com>
2015-04-06 22:44:30 +0700

    data for users, actions, g_d_object tables

--- GDManagerProject/app/database/seeds/DatabaseSeeder.php
+++ GDManagerProject/app/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,9 @@ class DatabaseSeeder extends Seeder {
 	{
 		Eloquent::unguard();
 
-		// $this->call('UserTableSeeder');
+		$this->call('UsersDataTableSeeder');
+		$this->call('ActionsDataTableSeeder');
+		$this->call('GDObjectTableSeeder');
 	}
 
 }
