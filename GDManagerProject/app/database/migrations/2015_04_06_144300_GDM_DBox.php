d6156770d2795413f4d4deb2161138f5ad473580
NPVinhLoc <vinhloc4927@gmail.com>
2015-04-06 21:55:52 +0700

    create tables in GDManager database

---
+++ GDManagerProject/app/database/migrations/2015_04_06_144300_GDM_DBox.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class GDMDBox extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('d_box', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->bigInteger('user_id')
+				->references('user_id')->on('user')->onDelete('cascade');
+			$table->string('token');
+			$table->date('expired_date');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('d_box');
+	}
+
+}
---
+++ GDManagerProject/app/database/migrations/2015_04_06_144805_GDM_Log.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class GDMLog extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('log', function(Blueprint $table)
+		{
+			$table->increments('log_id');
+			$table->bigInteger('user_id')
+				->references('user_id')->on('user')
+				->onDelete('cascade');
+			$table->bigInteger('action_id')
+				->references('action_id')->on('action')
+				->onDelete('cascade');
+			$table->string('filename');
+			$table->bigInteger('from')
+				->references('id')->on('g_d_object')
+				->onDelete('cascade');
+			$table->bigInteger('to')
+				->references('id')->on('g_d_object')
+				->onDelete('cascade');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('log');
+	}
+
+}
